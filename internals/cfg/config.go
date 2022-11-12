1c31a852abcae24d8dbbe943418d8d19277d65db
nzweb <nzweb@vk.com>
2022-11-12 19:52:27 +0300

    add base layout

--- internals/cfg/config.go
+++ internals/cfg/config.go
@@ -2,11 +2,11 @@ package cfg
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"log"
 )
 
-type Cfg struct {
+type Cfg struct { //наша структура для хранения конфигов, я полагаюсь на Viper для матчинга имен
 	Port   string
 	DbName string
 	DbUser string
@@ -16,26 +16,25 @@ type Cfg struct {
 }
 
 func LoadAndStoreConfig() Cfg {
-	v := viper.New()
-	v.SetEnvPrefix("SERF")
-	v.SetDefault("PORT", "8080")
+	v := viper.New()             //создаем экземпляр нашего ридера для Env
+	v.SetEnvPrefix("SERV")       //префикс, все переменные нашего сервера должны теперь стартовать с SERV_ для того, чтобы не смешиваться с системными
+	v.SetDefault("PORT", "8080") //ставим умолчальные настройки
 	v.SetDefault("DBUSER", "postgres")
 	v.SetDefault("DBPASS", "example")
 	v.SetDefault("DBHOST", "localhost")
 	v.SetDefault("DBPORT", "5432")
 	v.SetDefault("DBNAME", "postgres")
-	v.AutomaticEnv()
+	v.AutomaticEnv() //собираем наши переменные с системных
 
 	var cfg Cfg
 
-	err := v.Unmarshal(&cfg)
+	err := v.Unmarshal(&cfg) //закидываем переменные в cfg после анмаршалинга
 	if err != nil {
 		log.Panic(err)
 	}
-
 	return cfg
 }
 
-func (cfg *Cfg) GetDbString() string {
+func (cfg *Cfg) GetDBString() string { //маленький метод для сборки строки соединения с БД
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", cfg.DbUser, cfg.DbPass, cfg.DbHost, cfg.DbPort, cfg.DbName)
 }
